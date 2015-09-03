e368caf5b830d9d7e0217d6654d1ea2d532f99d2
Scott Nave <scott.nave@gmail.com>
2015-09-03 12:24:39 -0400

    Bug fixes

--- app/routes/submissions.php
+++ app/routes/submissions.php
@@ -247,7 +247,7 @@ $app->post('/submissionsave/:id', $authenticated(), function ($queueId) use ($ap
         1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
         2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
         3 => 'The uploaded file was only partially uploaded',
-        4 => 'No file was uploaded',
+        4 => 'Required file(s) were not uploaded',
         6 => 'Missing a temporary folder',
         7 => 'Failed to write file to disk.',
         8 => 'A PHP extension stopped the file upload.',
